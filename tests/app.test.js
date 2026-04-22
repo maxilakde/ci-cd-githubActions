be88a7034c40787260222dde6b947dd291b16daa
Maximiliano Astor <maximiliano.astor@gmail.com>
2026-04-22 00:15:50 +0000

    primera carga

---
+++ tests/app.test.js
@@ -0,0 +1,10 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe("GET /", ()=> {
+    it("debe responder con 200 y un mensaje", async() => {
+        const res = await request(app).get("/");
+        expect(res.statusCode).toBe(200);
+        expect(res.body.message).toBe("API Funcionando");
+    });
+});
