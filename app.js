a862a90c27fe66fea26016c6a8ed3ddeb659e928
Maximiliano Astor <maximiliano.astor@gmail.com>
2026-04-23 23:49:07 +0000

    nueva funcion ADMINISTRADORES

--- app.js
+++ app.js
@@ -13,5 +13,35 @@ app.get("/not_found", (req, res) => {
     res.status(404).json({ message: "Error not found" });
 })
 
+app.get("/administradores", (req, res) => {
+    res.status(200).json({ message: "Ok", administradores: [
+        {
+            "nombre": "Lisandro",
+            "apellido" : "Lopez",
+            "edad": "41"
+        },
+        {
+            "nombre": "Maravilla",
+            "apellido" : "Martinez",
+            "edad": "32"
+        },
+        {
+            "nombre": "Diego",
+            "apellido" : "Milito",
+            "edad": "45"
+        },
+        {
+            "nombre": "Gustavo",
+            "apellido" : "Costas",
+            "edad": "65"
+        },
+        {
+            "nombre": "Ezra",
+            "apellido" : "Sued",
+            "edad": "RIP"
+        }
+    ]  });
+})
+
 
 module.exports = app;
